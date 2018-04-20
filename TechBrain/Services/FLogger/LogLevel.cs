e44282e594d86bc56fedd22b2c2ed9a500956f7d
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-20 17:38:34 +0300

    Add FileLogger

---
+++ TechBrain/Services/FLogger/LogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechBrain.Services.FLogger
+{
+    public enum LogLevel
+    {
+        Info,
+        Debug,
+        Error
+    }
+
+}
---
+++ TechBrain/Services/FLogger/LoggerSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace TechBrain.Services.FLogger
+{
+    public class LoggerSettings
+    {
+        public LoggerSettings() { }
+
+        public string DateTimeFormat { get; set; } = "yyyy-MM-dd hh:mm:ss.fff";
+        public string FilePath { get; set; }
+        public string ArchiveFilePath { get; set; }
+        public string ArchiveDateFormat { get; set; } = "yyyyMMdd";
+        public int ArchiveMaxFiles { get; set; } = 10;
+        public bool EnableHttpTraceLog { get; set; }
+    }
+}
+
