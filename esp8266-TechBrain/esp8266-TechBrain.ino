0135eebdbda023165ca5e4498c45ad333187e847
Yegor <Yegorich_555@mail.ru>
2018-11-19 11:44:56 +0300

    Add output chip info

--- esp8266-TechBrain/esp8266-TechBrain.ino
+++ esp8266-TechBrain/esp8266-TechBrain.ino
@@ -1,12 +1,24 @@
 
+#define UART_BAUD 115200
 
 void setup(void) {
-  Serial.begin(115200);
-  Serial.printf("starting...\n");
-  
+  delay(10); //delay for debuging in ArduinoIDE
+  Serial.begin(UART_BAUD);
+  Serial.println("\nstarting...");
+
+  //chip info
+  uint32_t chipRealSize = ESP.getFlashChipRealSize();
+  uint32_t chipSize = ESP.getFlashChipSize();
+  if (chipRealSize != chipSize) {
+    Serial.printf("!!!Chip size (%u) is wrong. Real is %u\n", chipSize, chipRealSize);
+  }
+  Serial.printf("CpuFreq: %u MHz\n", ESP.getCpuFreqMHz());
+  Serial.printf("Flash speed: %u Hz\n", ESP.getFlashChipSpeed());
+  FlashMode_t ideMode = ESP.getFlashChipMode();
+  Serial.printf("Flash mode:  %s\n", (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"));
 }
 
 void loop(void) {
   delay(1000);
-  Serial.printf("loop\n");
+  Serial.println("loop");
 }
